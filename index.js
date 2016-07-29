3c5878b0fb03d6b4280d9a9477892f41c8157519
William Lu - Wish <wlu@Williams-MacBook-Air.local>
2016-07-29 01:28:40 -0700

    Push WitAI

--- index.js
+++ index.js
@@ -64,8 +64,8 @@ app.post('/webhook/', function (req, res) {
             sendGenericMessage(sender)
             continue
         }
-        search_query = client.message(text, {});
-        console.log("Search Query " + search_query);
+        // search_query = client.message(text, {});
+        // console.log("Search Query " + search_query);
         let url = 'https://wish.com/api/search?query=' + text
         request(url, function (error, response, body) {
             console.log('In request function')
