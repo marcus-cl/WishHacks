da67b4782b77a5c04995c7721cb8c6e5514dfad8
marcus-cl <marcus@contextlogic.com>
2016-07-29 00:18:37 -0700

    Push

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ app.post('/webhook/', function (req, res) {
             console.log('In request function')
             if (!error && response.statusCode == 200) {
 //                sendTextMessage(sender, "Text received, echo: Status Code = " + response.statusCode)
+                console.log('DATA: ' + body['data'])
                 data = body['data']['results'][0]
                 product = {}
                 product['img_url'] = data['img_url']
