65a522892b389e662a336ca41f4b2e326c5dcff6
marcus-cl <marcus@contextlogic.com>
2016-07-28 23:39:50 -0700

    Push

--- index.js
+++ index.js
@@ -44,8 +44,9 @@ app.post('/webhook/', function (req, res) {
         }
         let url = 'https://wish.com/api/contest/search?query=' + text
         request(url, function (error, response, body) {
+            console.
             if (!error && response.statusCode == 200) {
-                console.log(body) // Show the HTML for the Google homepage.
+                console.log('BODY: ' + body) // Show the HTML for the Google homepage.
                 sendTextMessage(sender, "Text received, echo: " + body)
             }
         })
