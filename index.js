b363018281805ec1c13723ef61527cfe1b15483f
marcus-cl <marcus@contextlogic.com>
2016-07-28 21:39:26 -0700

    Facebook Auth

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
+    res.send('933580406')
 })
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+    if (req.query['hub.verify_token'] === 'wish_chatbot_auth') {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
